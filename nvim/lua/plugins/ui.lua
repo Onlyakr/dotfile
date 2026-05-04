34240a3b3d382e99347b4d4f381906530b605c5d
Onlyakr <onlyakr2477@gmail.com>
2026-05-04 21:31:09 +0700

    Add Discord presence plugin (presence.nvim)

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -41,4 +41,21 @@ return {
       })
     end,
   },
+  {
+    "andweeb/presence.nvim",
+    config = function()
+      require("presence").setup({
+        auto_update = true,
+        neovim_image_text = "Neovim",
+        main_image = "neovim",
+        client_id = "793271441293967371",
+        log_level = nil,
+        debounce_timeout = 10,
+        enable_line_number = false,
+        blacklist = {},
+        buttons = true,
+        show_time = true,
+      })
+    end,
+  },
 }
